3efffb806353c0cf41329a7c38ec7a5f7433b913
Brandon <7310114+munyb@users.noreply.github.com>
2022-03-24 21:14:24 -0700

    Hardcode Sentry URI

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app.config.from_object('config.DevelopmentConfig')
 db = SQLAlchemy(app)
 
 sentry_sdk.init(
-    dsn=app.config.SENTRY_DSN_URI,
+    dsn='https://bbf1e21e7ed04190a0ff5be402ca21b4@o983081.ingest.sentry.io/5938602',
     integrations=[FlaskIntegration()],
 
     # Set traces_sample_rate to 1.0 to capture 100%

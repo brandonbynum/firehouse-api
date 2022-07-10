497c78a32598abaadf7b360798a1e45e10fe7f11
Brandon <7310114+brandonbynum@users.noreply.github.com>
2022-07-10 10:23:23 -0700

    feat(event-summary-update): Update date format and capitalize metro areas

--- api/services/event_services.py
+++ api/services/event_services.py
@@ -13,7 +13,8 @@ from models import Genres
 from models import MetropolitanArea
 from models import Venues
 
-class EventService():
+
+class EventService:
     # def __init__(self):
     #     self.Artist = models.Artist
     #     self.ArtistGenre = models.ArtistGenre
@@ -23,7 +24,7 @@ class EventService():
     #     self.Genres = models.Genres
     #     self.MetropolitanArea = models.MetropolitanArea
     #     self.Venues = models.Venues
-        
+
     # TODO: Filter date after now
     def upcoming_events_test(self, metro=None):
         all_filters = [Events.date >= datetime.today()]
@@ -32,96 +33,111 @@ class EventService():
         if metro:
             all_filters.append(func.lower(MetropolitanArea.name) == metro.lower())
 
-        print(f'----------METRO: {metro} ---------------')
-                        
-        query = Events.query\
-            .filter(*all_filters)\
-            .join(EventArtist, Events.id == EventArtist.event_id)\
-            .join(Artist, EventArtist.artist_id == Artist.id)\
-            .join(ArtistGenre, Artist.id == ArtistGenre.artist_id)\
-            .join(Genres, ArtistGenre.genre_id == Genres.id)\
-            .join(Venues, Events.venue_id == Venues.id)\
-            .join(Cities, Venues.city_id == Cities.id)\
-            .join(MetropolitanArea, Cities.metropolitan_id == MetropolitanArea.id)\
-            .order_by(Events.date)\
+        print(f"----------METRO: {metro} ---------------")
+
+        query = (
+            Events.query.filter(*all_filters)
+            .join(EventArtist, Events.id == EventArtist.event_id)
+            .join(Artist, EventArtist.artist_id == Artist.id)
+            .join(ArtistGenre, Artist.id == ArtistGenre.artist_id)
+            .join(Genres, ArtistGenre.genre_id == Genres.id)
+            .join(Venues, Events.venue_id == Venues.id)
+            .join(Cities, Venues.city_id == Cities.id)
+            .join(MetropolitanArea, Cities.metropolitan_id == MetropolitanArea.id)
+            .order_by(Events.date)
             .add_columns(
-                Events.id, 
-                Events.name.label('event_name'),
-                Events.date, 
+                Events.id,
+                Events.name.label("event_name"),
+                Events.date,
                 Events.type,
-                Events.tickets_link, 
+                Events.tickets_link,
                 EventArtist.headliner,
-                Artist.name.label('artist_name'),
-                Cities.name.label('city_name'),
-                Genres.name.label('genre_name'),
-                Venues.name.label('venue_name'),
-            ).all()
+                Artist.name.label("artist_name"),
+                Cities.name.label("city_name"),
+                Genres.name.label("genre_name"),
+                Venues.name.label("venue_name"),
+            )
+            .all()
+        )
 
         for raw_event in query:
             # Check if event_id/artist.name exists in the list
             # If so, add genre to existing item
             # If not, add item to list
-            print('raw_event:', raw_event.id, raw_event.artist_name)
             for hashIndex, hash_event in enumerate(event_list):
-                print('hash_event', hash_event['event_id'], hash_event['artist']['name'], '\n')
-                if raw_event.id == hash_event['event_id'] and raw_event.artist_name == hash_event['artist']['name']:
-                    event_list[hashIndex]['genres'].append(raw_event.genre_name)
+                if raw_event.id == hash_event["event_id"] and raw_event.artist_name == hash_event["artist"]["name"]:
+                    event_list[hashIndex]["genres"].append(raw_event.genre_name)
                     break
             else:
-                event_list.append({
-                    'event_id': raw_event.id,
-                    'event_name': raw_event.event_name,
-                    'artist': {
-                        'name': raw_event.artist_name,
-                        'headliner': raw_event.headliner
-                    },
-                    'city': raw_event.city_name,
-                    'genres': [raw_event.genre_name],
-                    'date': raw_event.date.strftime("%m/%d/%Y"),
-                    'tickets_url': raw_event.tickets_link,
-                    'type': raw_event.type,
-                    'venue': raw_event.venue_name,
-                })
+                event_list.append(
+                    {
+                        "event_id": raw_event.id,
+                        "event_name": raw_event.event_name,
+                        "artist": {"name": raw_event.artist_name, "headliner": raw_event.headliner},
+                        "city": raw_event.city_name,
+                        "genres": [raw_event.genre_name],
+                        "date": raw_event.date.strftime("%a, %b %-d"),
+                        "tickets_url": raw_event.tickets_link,
+                        "type": raw_event.type,
+                        "venue": raw_event.venue_name,
+                    }
+                )
         return event_list
-    
+
     def get_event_details(id_to_query):
         artists = []
         event_details = []
-        query_res = Events.query.filter(Events.id == id_to_query)\
-            .join(EventArtist, Events.id == EventArtist.event_id)\
-            .join(Artist, EventArtist.artist_id == Artist.id)\
-            .join(Venues, Events.venue_id == Venues.id)\
-            .join(Cities, Venues.city_id == Cities.id)\
-            .join(MetropolitanArea, Cities.metropolitan_id == MetropolitanArea.id)\
-            .add_columns(Events.id, Events.date, Events.type, Events.start_at,
-                        Events.end_at, Events.name, Events.tickets_link,
-                        Artist.name.label('artist_name'), EventArtist.headliner, Cities.name.label('city_name'), Cities.state, 
-                        MetropolitanArea.name.label('metropolitan_name'), Venues.name.label('venue_name'), Venues.address)
-        
+        query_res = (
+            Events.query.filter(Events.id == id_to_query)
+            .join(EventArtist, Events.id == EventArtist.event_id)
+            .join(Artist, EventArtist.artist_id == Artist.id)
+            .join(Venues, Events.venue_id == Venues.id)
+            .join(Cities, Venues.city_id == Cities.id)
+            .join(MetropolitanArea, Cities.metropolitan_id == MetropolitanArea.id)
+            .add_columns(
+                Events.id,
+                Events.date,
+                Events.type,
+                Events.start_at,
+                Events.end_at,
+                Events.name,
+                Events.tickets_link,
+                Artist.name.label("artist_name"),
+                EventArtist.headliner,
+                Cities.name.label("city_name"),
+                Cities.state,
+                MetropolitanArea.name.label("metropolitan_name"),
+                Venues.name.label("venue_name"),
+                Venues.address,
+            )
+        )
+
         for row in query_res:
             event_details.append(row)
-            artists.append({
-                'name': row.artist_name,
-                'headliner': row.headliner,
-            })
-            artists.sort(key=lambda artist: artist['headliner'], reverse=True)
-        
+            artists.append(
+                {
+                    "name": row.artist_name,
+                    "headliner": row.headliner,
+                }
+            )
+            artists.sort(key=lambda artist: artist["headliner"], reverse=True)
+
         res = {
-            'event_id': query_res[0].id,
-            'artists': artists,
-            'date': query_res[0].date,
-            'end_at': query_res[0].end_at and str(query_res[0].end_at),
-            'name': query_res[0].name,
-            'metropolitan_area': query_res[0].metropolitan_name,
-            'start_at': query_res[0].start_at and str(query_res[0].start_at),
-            'tickets_url': query_res[0].tickets_link,
-            'type': query_res[0].type,
-            'venue': {
-                'name': query_res[0].venue_name,
-                'address': query_res[0].address,
-                'city': query_res[0].city_name,
-                'state': query_res[0].state,
-            }
+            "event_id": query_res[0].id,
+            "artists": artists,
+            "date": query_res[0].date,
+            "end_at": query_res[0].end_at and str(query_res[0].end_at),
+            "name": query_res[0].name,
+            "metropolitan_area": query_res[0].metropolitan_name,
+            "start_at": query_res[0].start_at and str(query_res[0].start_at),
+            "tickets_url": query_res[0].tickets_link,
+            "type": query_res[0].type,
+            "venue": {
+                "name": query_res[0].venue_name,
+                "address": query_res[0].address,
+                "city": query_res[0].city_name,
+                "state": query_res[0].state,
+            },
         }
 
         return res
